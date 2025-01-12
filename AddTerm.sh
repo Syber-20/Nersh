10a62a6d7b66548302256dad2cf02f9eff1bdca6
p@ren <ntimprosper308@gmail.com>
2025-01-12 07:04:40 +0000

    Edited

--- AddTerm.sh
+++ AddTerm.sh
@@ -53,4 +53,20 @@ case $editor_choice in
 esac
 
 echo "HTML file created at $html_file_path"
+
+# Ask user if they want to commit the changes
+read -p "Do you want to commit the new term added? (yes/no): " commit_choice
+
+if [[ "$commit_choice" == "yes" ]]; then
+    # Stage all changes
+    git add .
+
+    # Commit with a meaningful message
+    commit_message="Add term: $term"
+    git commit -m "$commit_message" -m "Added new term '$term' and updated related files."
+    echo "Changes committed."
+else
+    echo "Changes not committed."
+fi
+
 echo "Done!"
